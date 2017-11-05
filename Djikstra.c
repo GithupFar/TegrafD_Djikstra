924b938b3e74293c02650fac34938aec3dad19a9
Felicitation_ww <fafaaidah@gmail.com>
2017-11-05 10:17:00 +0700

    Commit baru, C algorithm , vertex masih default 9

--- Djikstra.c
+++ Djikstra.c
@@ -94,7 +94,7 @@ void printPath(int parent[], int j)
 
 int main()
 {
-    int v,x,y;
+    int v,x,y,dst;
     //printf("Masukkan jumlah vertexnya :");
     //scanf("%d", &V);
     int graph[V][V];
@@ -103,20 +103,11 @@ int main()
             scanf("%d", &graph[y][x]);
         }
     }
-
-	/* //Let us create the example graph discussed above
-	int graph[V][V] = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
-					{4, 0, 8, 0, 0, 0, 0, 11, 0},
-					{0, 8, 0, 7, 0, 4, 0, 0, 2},
-					{0, 0, 7, 0, 9, 14, 0, 0, 0},
-					{0, 0, 0, 9, 0, 10, 0, 0, 0},
-					{0, 0, 4, 0, 10, 0, 2, 0, 0},
-					{0, 0, 0, 14, 0, 2, 0, 1, 6},
-					{8, 11, 0, 0, 0, 0, 1, 0, 7},
-					{0, 0, 2, 0, 0, 0, 6, 7, 0}
-					};*/
-
-	dijkstra(graph, 0);//, V);
+    
+    printf("Masukkan vertex yang menjadi tujuan awal: ");
+    scanf("%d",&dst);
+	
+	dijkstra(graph, dst);//, V);
 
 	return 0;
 }
